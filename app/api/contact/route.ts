9abc65819c3792f811fa2ae4c0018a0acb0426ee
humanediedofanxiety <saysayeem.pr@gmail.com>
2025-01-25 22:40:17 +0600

    fixed contact submission bug

--- app/api/contact/route.ts
+++ app/api/contact/route.ts
@@ -7,6 +7,7 @@ export async function POST(req: Request) {
 	const transporter = nodemailer.createTransport({
 		host: process.env.EMAIL_SERVER,
 		port: Number.parseInt(process.env.EMAIL_PORT || "587"),
+		secure: false,
 		auth: {
 			user: process.env.EMAIL_USER,
 			pass: process.env.EMAIL_PASSWORD,
